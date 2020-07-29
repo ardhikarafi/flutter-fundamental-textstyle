89191587ee1b6492fdd1fac61bd1cbf82e8ae387
Your Name <you@example.com>
2020-07-29 18:49:34 +0700

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan Text Style"),
+      ),
+      body: Center(
+        child: Text(
+          "Ini adalah Text",
+          style: TextStyle(
+              fontFamily: "Roboto_cus",
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.overline,
+              decorationColor: Colors.red,
+              decorationThickness: 5,
+              decorationStyle: TextDecorationStyle.wavy),
+        ),
+      ),
+    ));
+  }
+}
